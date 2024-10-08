44f7c0f4c5107133deb92b951c1a858a1d604ec2
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2024-10-08 16:02:42 -0500

    delete js files

---
+++ 5.4+Color+Vocab+Project/app.js
@@ -0,0 +1,11 @@
+// const circle = document.querySelectorAll("img");
+// console.log(circle);
+// circle.style.borderRadius="100px";
+// circle.setAtribute("color", "black");
+
+const circle = document.querySelector("img");
+circle.style.borderRadius = "100px";
+
+const title = document.querySelector(".title");
+title.style.border = "3px solid black";
+title.style.padding = "10px";
