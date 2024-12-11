21d0b909ff715876ff7bf48a19d7a32a810f3532
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2024-12-11 14:23:45 -0500

    human years in ages dog

---
+++ 12.1 JavaScript/ageDog.js
@@ -0,0 +1,4 @@
+dogAge = prompt("what´s your age´s dog?");
+humanAge = ((dogAge - 2) * 4 ) + 21;
+alert("The age of your dog in human years is: " + humanAge);
+// run in the source of chrome developer tools fromm now on!
