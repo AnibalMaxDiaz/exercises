c75a428c74b14a14c1b6044358bb3dc8d33c7ddb
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2025-02-03 17:29:22 -0500

    loveCalculator

---
+++ 12.4 love-calculator/loveCalculator.js
@@ -0,0 +1,6 @@
+var name1 = prompt("write the first name");
+var name2 = prompt("write the second name");
+var number = Math.random();
+number *= 100;
+number = Math.floor(number)+1;
+alert(`The percentage of compatibility is ${number} %`);
