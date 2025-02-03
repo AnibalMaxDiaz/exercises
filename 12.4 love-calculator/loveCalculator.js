b61ce060f15760e2b3a537b31aca75c548124666
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2025-02-03 17:57:19 -0500

    add conditional if else

--- 12.4 love-calculator/loveCalculator.js
+++ 12.4 love-calculator/loveCalculator.js
@@ -1,6 +1,10 @@
-var name1 = prompt("write the first name");
-var name2 = prompt("write the second name");
+prompt("write the first name");
+prompt("write the second name");
 var number = Math.random();
 number *= 100;
 number = Math.floor(number)+1;
-alert(`The percentage of compatibility is ${number} %`);
+if (number > 70) {
+  alert(`The percentage of compatibility is ${number} %, and that´s great!`);
+} else {
+  alert(`The percentage of compatibility is ${number} %`);
+}
