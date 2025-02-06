3b9002c890813d02b129fd6be42491544617f1e6
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2025-02-06 05:23:29 -0500

    add conditionals

--- 12.3 bmiCalculator/bmiCalculator.js
+++ 12.3 bmiCalculator/bmiCalculator.js
@@ -1,6 +1,14 @@
-function bmiCalculator(kg, m){
-  var bmi = Math.round(kg/(m*m));
-  return bmi;
+function bmiCalculator (weight, height) {
+  var bmi = weight/Math.pow(height, 2);
+  //bmi = Math.floor(bmi);
+  if (bmi < 18.5) {
+    return("Your BMI is " + bmi + ", so you are underweight.");
+  }
+if (bmi > 18.5 && bmi < 24.9) {
+    return("Your BMI is " + bmi + ", so you have a normal weight.");
+  }
+if (bmi > 24.9) {
+    return("Your BMI is " + bmi + ", so you are overweight.");
+  }
 }
-
-console.log( bmiCalculator(65, 1.8));
+bmiCalculator(65, 1.78);
