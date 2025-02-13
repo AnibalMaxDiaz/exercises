fcbefba18ba8a812de2811ca8684e6675dd5d42e
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2025-02-12 21:18:33 -0500

    part 2

--- 13.2 Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ 13.2 Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,7 +1,28 @@
-var botones = document.querySelectorAll(".drum");
-botones.forEach((boton)=>{boton.addEventListener("click", ()=>{
-  alert("You did click");
-  })
-})
+// another way to do it!
+// const array = document.querySelectorAll("button");
+// for (let I = 0; I < array.length; I++) {
+//   array[I].addEventListener("click", function() {
+//     console.log("I got clicked!");
+//   });
+  
+// }
+
+ var botones = document.querySelectorAll(".drum");
+ botones.forEach((boton)=>{boton.addEventListener("click", ()=>{
+   alert("I got clicked!");
+   })
+ })
+
+
+
+
+
+
+
+
+
+
+
+
 
 
