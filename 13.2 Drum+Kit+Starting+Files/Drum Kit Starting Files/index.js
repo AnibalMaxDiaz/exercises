4dfb8f27ae94810b3626a45546a8050089923c9f
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2025-02-15 18:57:33 -0500

    add change of color

--- 13.2 Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ 13.2 Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -7,13 +7,30 @@
   
 // }
 
- var botones = document.querySelectorAll(".drum");
- botones.forEach((boton)=>{boton.addEventListener("click", ()=>{
-   alert("I got clicked!");
-   })
- })
+//  var botones = document.querySelectorAll(".drum");
+//  botones.forEach((boton)=>{boton.addEventListener("click", ()=>{
+//    alert("I got clicked!");
+//    })
+//  })
 
+ 
+//  var botones = document.querySelectorAll(".drum");
+//  botones.forEach((boton)=>{boton.addEventListener("click", ()=>{
+//   var audio = new Audio("sounds/tom-1.mp3");
+//   audio.play();
+//   // en las funciones flecha no sirve el this
+//     this.style.color = "white";
+//    })
+//  })
 
+var botones = document.querySelectorAll(".drum");
+botones.forEach((boton) => {
+  boton.addEventListener("click", function() {
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+    this.style.color = "blue";
+  });
+});
 
 
 
